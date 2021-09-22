d7e7d572fe381ac5053153550127b2d56d7a053f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-22 06:36:21 -0400

    feat(<Header />): add a Navbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,11 +4,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 function Footer() {
 	return (
 		<footer>
-			<Row>
-				<Col className='text-center py-3'>
-					&copy; 2021 Thumb Technology Consulting LLC
-				</Col>
-			</Row>
+			<Container>
+				<Row>
+					<Col className='text-center py-3'>
+						&copy; 2021 Thumb Technology Consulting LLC
+					</Col>
+				</Row>
+			</Container>
 		</footer>
 	);
 }
