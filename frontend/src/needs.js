6296653c483199522280fff6f4af905e9479fc99
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-23 06:30:53 -0400

    feat(needs.js): create an array of items to mock API with

---
+++ frontend/src/needs.js
@@ -0,0 +1,69 @@
+const needs = [
+	{
+		_id: '1',
+		name: 'Lentils (5 lbs. bag)',
+		image: '/images/lentils.png',
+		description:
+			'Lentils are relatively quick and easy to prepare, and their low cost makes them an accessible form of high quality protein for many people around the world.',
+		brand: 'N/A',
+		category: 'Food',
+		price: 9.99,
+		countInStock: 10,
+		rating: 4.5,
+		numReviews: 12,
+	},
+	{
+		_id: '2',
+		name: 'Wool Blanket',
+		image: '/images/blanket.png',
+		description:
+			'Wool has millions of air pockets that trap warmth. It is a soothing, natural warmth, yet light in weight. It wicks away body moisture for dry comfort.',
+		brand: 'N/A',
+		category: 'Safety',
+		price: 19.99,
+		countInStock: 7,
+		rating: 4.0,
+		numReviews: 8,
+	},
+	{
+		_id: '3',
+		name: 'Fava Beans (3 lbs. bag)',
+		image: '/images/beans.png',
+		description:
+			'Fava beans are loaded with vitamins, minerals, fiber and protein.',
+		brand: 'N/A',
+		category: 'Food',
+		price: 6.99,
+		countInStock: 5,
+		rating: 3,
+		numReviews: 12,
+	},
+	{
+		_id: '4',
+		name: 'Dates (2 lbs. bag)',
+		image: '/images/dates.png',
+		description:
+			'Date fruits have been present in the Middle East and Northern Africa as a local food for more than 7000 years. They have been agricultural produced for 5000 years. Date fruits are very nutritious and have lots of vitamins (A, B, D) and sugar (fructose), what makes them great source of energy.',
+		brand: 'N/A',
+		category: 'Food',
+		price: 8.99,
+		countInStock: 11,
+		rating: 5,
+		numReviews: 12,
+	},
+	{
+		_id: '5',
+		name: 'Water Bottles (12-pack)',
+		image: '/images/water_bottle.png',
+		description:
+			'Egypt is suffering from severe water scarcity. Only 20 cubic meters of water per person of internal renewable freshwater resources remain.',
+		brand: 'N/A',
+		category: 'Food',
+		price: 13.99,
+		countInStock: 7,
+		rating: 3.5,
+		numReviews: 10,
+	},
+];
+
+export default needs;
