fb373cddb4fe816ed28c61b826bf714aaca10b21
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-23 05:47:04 -0400

    feat(<Header />): add font-awesome-icon for user

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,7 +13,9 @@ function Header() {
 							<Nav.Link href='/cart'>
 								<i className='fas fa-shopping-cart px-2'></i>Cart
 							</Nav.Link>
-							<Nav.Link href='/login'>Login</Nav.Link>
+							<Nav.Link href='/login'>
+								<i className='fas fa-user px-2'></i>Login
+							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
