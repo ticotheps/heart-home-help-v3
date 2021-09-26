c71652ba11907ad937e520f4cd6019741fab75da
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-26 16:57:54 -0400

    refactor(<Header />): replace 'href' for cart and login links with <LinkContainer> tag

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,12 +13,16 @@ function Header() {
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='mr-auto'>
-							<Nav.Link href='/cart'>
-								<i className='fas fa-shopping-cart px-2'></i>Cart
-							</Nav.Link>
-							<Nav.Link href='/login'>
-								<i className='fas fa-user px-2'></i>Login
-							</Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<i className='fas fa-shopping-cart px-2'></i>Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<i className='fas fa-user px-2'></i>Login
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
