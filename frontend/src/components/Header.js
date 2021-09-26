624763496618612b0a87f57a24656d19f9a40d3e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-26 16:53:53 -0400

    refactor(<Header />): replace home page 'href' with <LinkContainer>

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='/'>Home</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand>Heart. Home. Help.</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='mr-auto'>
