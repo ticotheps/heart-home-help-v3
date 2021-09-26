d1c24a8b389f6557c08aaa69a63301b12dbb8e52
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-09-26 17:14:28 -0400

    feat(<NeedScreen />): create layout for need-product's detail page

--- frontend/src/screens/NeedScreen.js
+++ frontend/src/screens/NeedScreen.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
+import Rating from '../components/Rating';
 
-export default function NeedScreen() {
-	return <div>Need</div>;
+import needs from '../needs';
+
+export default function NeedScreen({ match }) {
+	const need = needs.find((need) => need._id == match.params.id);
+	return (
+		<div>
+			<Link to='/' className='btn btn-light my-3'>
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={need.image} alt={need.name} fluid />
+				</Col>
+			</Row>
+		</div>
+	);
 }
