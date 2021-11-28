ea0c87f4374202a8b650a27e9d8a6eb9faa467d8
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-11-28 16:23:51 -0500

    feat(views): create new function-based view for retrieving 'needs''

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from .needs import needs
 
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/needs/',
+        '/api/needs/create/',
+        
+        '/api/needs/upload/',
+        
+        '/api/needs/top/',
+        '/api/needs/<id>/',
+        
+        '/api/needs/delete/<id>/',
+        '/api/needs/<update>/<id>/',
+    ]
+    return JsonResponse(routes, safe=False)
+
+def getNeeds(request):
+    return JsonResponse(needs, safe=False)
