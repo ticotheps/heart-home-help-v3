1748517f18c3097f33a81169228bdbaac0b7215b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-11-29 21:08:45 -0500

    feat(settings.py): configure settings to allow "http://127.0.0.1:3000/" and "http://localhost:3000" to access API

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,4 +126,10 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+# CORS_ALLOW_ALL_ORIGINS = True
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
