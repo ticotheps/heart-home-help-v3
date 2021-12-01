c17edd83decd9d6d061c6fe3a0408d894135b026
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-11-30 22:10:31 -0500

    fix(<Need />): change the 'to' attribute of the <Link /> for each need on the needs page to be '/needs/...' instead of '/need/...'

--- frontend/src/components/Need.js
+++ frontend/src/components/Need.js
@@ -9,12 +9,12 @@ export default function Need({ need }) {
 			style={{ width: '16rem', height: '24rem' }}
 			className='my-3 p-3 rounded need-card'
 		>
-			<Link to={`/need/${need._id}`}>
+			<Link to={`/needs/${need._id}`}>
 				<Card.Img className='card-image' src={need.image} />
 			</Link>
 
 			<Card.Body>
-				<Link to={`/need/${need._id}`}>
+				<Link to={`/needs/${need._id}`}>
 					<Card.Title as='div'>
 						<strong>{need.name}</strong>
 					</Card.Title>
