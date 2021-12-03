c75a7d88e06fe5d532544742df8cec96fb534adb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-02 21:00:44 -0500

    feat(models): create a new 'Review' model

--- backend/base/models.py
+++ backend/base/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Need(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -19,3 +18,15 @@ class Need(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class Review(models.Model):
+    need = models.ForeignKey(Need, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.rating)
