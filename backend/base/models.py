08e126c2a6f066b5cf8c3223415d854c5bb2834f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-06 23:43:07 -0500

    feat(models): create a model for "OrderItem"

--- backend/base/models.py
+++ backend/base/models.py
@@ -47,3 +47,16 @@ class Order(models.Model):
     
     def __str__(self):
         return str(self.createdAt)
+    
+    
+class OrderItem(models.Model):
+    need = models.ForeignKey(Need, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.name)
