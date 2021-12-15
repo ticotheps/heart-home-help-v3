f64c40b9dd54988505ab89039fb181738d372e03
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-14 23:43:32 -0500

    feat(store.js): combine new 'needListReducer' with other reducers in Redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { needListReducer } from './reducers/needReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+	needList: needListReducer,
+});
 
 const initialState = {};
 
