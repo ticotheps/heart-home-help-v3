0f1967e827ce6634d1c4ad5a67c05fefd212d671
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-18 22:01:52 -0500

    feat(reducers): create a new reducer for cart items

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { needListReducer, needDetailsReducer } from './reducers/needReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
 	needList: needListReducer,
 	needDetails: needDetailsReducer,
+	cart: cartReducer,
 });
 
 const initialState = {};
