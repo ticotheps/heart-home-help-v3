bdcd9e9e6398477c259bd8ddc1078ccf7dd09aa3
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-18 23:16:29 -0500

    feat(<CartScreen />): retrieve the 'cart' state from the Redux store and into the <CartScreen />

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -20,6 +20,10 @@ export default function CartScreen({ match, location, history }) {
 
 	const dispatch = useDispatch();
 
+	const cart = useSelector((state) => state.cart);
+	const { cartItems } = cart;
+	console.log({ cartItems });
+
 	useEffect(() => {
 		if (needId) {
 			dispatch(addToCart(needId, qty));
