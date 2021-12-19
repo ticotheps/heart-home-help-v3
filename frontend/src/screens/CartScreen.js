8552fdf8f664c39f00415d3c5a422bb410640f44
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-18 23:11:40 -0500

    feat(<CartScreen />): use the 'match' and the 'location' props to find the item ID and the quantity for updating state of the cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	Row,
+	Col,
+	ListGroup,
+	Image,
+	Form,
+	Button,
+	Card,
+} from 'react-bootstrap';
+import { Message } from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
-export default function CartScreen() {
+export default function CartScreen({ match, location, history }) {
+	const needId = match.params.id;
+
+	const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (needId) {
+			dispatch(addToCart(needId, qty));
+		} else {
+		}
+	}, [dispatch, needId, qty]);
+
 	return (
 		<div>
 			<h4>Cart</h4>
