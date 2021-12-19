0d8e4b46a382b97e9b201801d479c7258f0b2ffc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-18 19:52:51 -0500

    feat(actions): create an action for listing a need's details

--- frontend/src/actions/needActions.js
+++ frontend/src/actions/needActions.js
@@ -3,6 +3,9 @@ import {
 	NEED_LIST_REQUEST,
 	NEED_LIST_SUCCESS,
 	NEED_LIST_FAIL,
+	NEED_DETAILS_REQUEST,
+	NEED_DETAILS_SUCCESS,
+	NEED_DETAILS_FAIL,
 } from '../constants/needConstants';
 
 export const listNeeds = () => async (dispatch) => {
@@ -18,6 +21,27 @@ export const listNeeds = () => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: NEED_LIST_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const listNeedDetails = (id) => async (dispatch) => {
+	try {
+		dispatch({ type: NEED_DETAILS_REQUEST });
+
+		const { data } = await axios.get(`/api/needs/${id}`);
+
+		dispatch({
+			type: NEED_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: NEED_DETAILS_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
