8076ce37dcc6fa70dea6e804346b7c0fda5e5f73
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-18 19:44:05 -0500

    feat(reducers): create a reducer for a specific need's details
    These details will be rendered in the <Need />.

--- frontend/src/reducers/needReducers.js
+++ frontend/src/reducers/needReducers.js
@@ -2,6 +2,9 @@ import {
 	NEED_LIST_REQUEST,
 	NEED_LIST_SUCCESS,
 	NEED_LIST_FAIL,
+	NEED_DETAILS_REQUEST,
+	NEED_DETAILS_SUCCESS,
+	NEED_DETAILS_FAIL,
 } from '../constants/needConstants';
 
 export const needListReducer = (state = { needs: [] }, action) => {
@@ -13,6 +16,25 @@ export const needListReducer = (state = { needs: [] }, action) => {
 			return { loading: false, needs: action.payload };
 
 		case NEED_LIST_FAIL:
+			return { loading: false, error: action.payload };
+
+		default:
+			return state;
+	}
+};
+
+export const needDetailsReducer = (
+	state = { need: { reviews: [] } },
+	action
+) => {
+	switch (action.type) {
+		case NEED_DETAILS_REQUEST:
+			return { loading: true, ...state };
+
+		case NEED_DETAILS_SUCCESS:
+			return { loading: false, need: action.payload };
+
+		case NEED_DETAILS_FAIL:
 			return { loading: false, error: action.payload };
 
 		default:
