6a5d3c6072aebdb3e899c441a476d20e66cdd4eb
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-12-21 10:13:08 -0500

    feat(<CartScreen />): enable users to remove items from their cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
 	switch (action.type) {
@@ -10,8 +10,8 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 				return {
 					...state,
 					cartItems: state.cartItems.map((x) =>
-						// 'need' here does not refer to an object, but rather
-						// the ID of an object.
+						// 'need' does not refer to an object here, but rather
+						// to the ID of an object.
 						x.need === existItem.need ? item : x
 					),
 				};
@@ -21,6 +21,13 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 					cartItems: [...state.cartItems, item],
 				};
 			}
+
+		case CART_REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: state.cartItems.filter((x) => x.need !== action.payload),
+			};
+
 		default:
 			return state;
 	}
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
 	Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 export default function CartScreen({ match, location, history }) {
 	const needId = match.params.id;
@@ -26,9 +26,19 @@ export default function CartScreen({ match, location, history }) {
 	useEffect(() => {
 		if (needId) {
 			dispatch(addToCart(needId, qty));
-		} else {
 		}
 	}, [dispatch, needId, qty]);
+
+	const removeFromCartHandler = (id) => {
+		dispatch(removeFromCart(id));
+		// removes the previously-added query string from the URL to prevent
+		// an unwanted update of the cart on a refresh of the page
+		history.push('/cart');
+	};
+
+	const checkoutHandler = () => {
+		history.push('/login?redirect=shipping');
+	};
 
 	return (
 		<Row>
@@ -42,21 +52,85 @@ export default function CartScreen({ match, location, history }) {
 					<ListGroup variant='flush'>
 						{cartItems.map((item) => (
 							<ListGroup.Item key={item.need}>
-								<Row>
+								<Row className='align-items-center'>
 									<Col md={2}>
 										<Image src={item.image} alt={item.name} fluid rounded />
 									</Col>
-									<Col md={3}>
-										<Link to={`/need/${item.need}`}>{item.name}</Link>
+
+									<Col md={4}>
+										<Link to={`/needs/${item.need}`}>{item.name}</Link>
 									</Col>
+
 									<Col md={2}>${item.price}</Col>
+
+									<Col md={1}>
+										<span>Qty: </span>
+									</Col>
+
+									<Col md={1}>
+										<Form.Control
+											as='select'
+											value={item.qty}
+											onChange={(e) => {
+												dispatch(addToCart(item.need, Number(e.target.value)));
+												// removes the previously-added query string from the URL to prevent
+												// an unwanted update of the cart on a refresh of the page
+												history.push('/cart');
+											}}
+										>
+											{[...Array(item.countInStock).keys()].map((x) => (
+												<option key={x + 1} value={x + 1}>
+													{x + 1}
+												</option>
+											))}
+										</Form.Control>
+									</Col>
+
+									<Col md={1}>
+										<Button
+											type='button'
+											variant='light'
+											onClick={() => removeFromCartHandler(item.need)}
+										>
+											<i className='fas fa-trash'></i>
+										</Button>
+									</Col>
 								</Row>
 							</ListGroup.Item>
 						))}
 					</ListGroup>
 				)}
 			</Col>
-			<Col md={4}></Col>
+			<Col md={4}>
+				<Card>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>
+								Subtotal for{' '}
+								{cartItems.reduce((acc, item) => acc + item.qty, 0)} item(s):
+							</h2>
+							<h3>
+								$
+								{cartItems
+									.reduce((acc, item) => acc + item.qty * item.price, 0)
+									.toFixed(2)}
+							</h3>
+						</ListGroup.Item>
+
+						<ListGroup.Item className='text-center'>
+							<Button
+								type='button'
+								className='btn-block btn-dark'
+								size='lg'
+								disabled={cartItems.length === 0}
+								onClick={checkoutHandler}
+							>
+								Proceed to Checkout
+							</Button>
+						</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</Col>
 		</Row>
 	);
 }
